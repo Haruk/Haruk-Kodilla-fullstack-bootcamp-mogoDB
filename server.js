9744666824c542f76da60dee7661780f65fb28a2
Aleksandra <haruka.lex@gmail.com>
2023-03-11 22:03:59 +0100

    Add enpoints tests

--- server.js
+++ server.js
@@ -1,10 +1,10 @@
-const express = require("express");
-const cors = require("cors");
-const mongoose = require("mongoose");
+const express = require('express');
+const cors = require('cors');
+const mongoose = require('mongoose');
 
-const employeesRoutes = require("./routes/employees.routes");
-const departmentsRoutes = require("./routes/departments.routes");
-const productsRoutes = require("./routes/products.routes");
+const employeesRoutes = require('./routes/employees.routes');
+const departmentsRoutes = require('./routes/departments.routes');
+const productsRoutes = require('./routes/products.routes');
 
 const app = express();
 
@@ -12,24 +12,38 @@ app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
-app.use("/api", employeesRoutes);
-app.use("/api", departmentsRoutes);
-app.use("/api", productsRoutes);
+app.use('/api', employeesRoutes);
+app.use('/api', departmentsRoutes);
+app.use('/api', productsRoutes);
 
 app.use((req, res) => {
-  res.status(404).send({ message: "Not found..." });
+  res.status(404).send({ message: 'Not found...' });
 });
 
-mongoose.connect("mongodb://localhost:27017/companyDB", {
-  useNewUrlParser: true,
-});
+// connects our backend code with the database
+
+const NODE_ENV = process.env.NODE_ENV;
+let dbUri = '';
+
+if (NODE_ENV === 'production') dbUri = 'url to remote db';
+else if (NODE_ENV === 'test') dbUri = 'mongodb://localhost:27017/companyDBtest';
+else dbUri = 'mongodb://localhost:27017/companyDB';
+
+mongoose.connect(dbUri, { useNewUrlParser: true, useUnifiedTopology: true });
 const db = mongoose.connection;
 
-db.once("open", () => {
-  console.log("Connected to the database");
-});
-db.on("error", (err) => console.log("Error " + err));
+// mongoose.connect('mongodb://localhost:27017/companyDB', {
+//   useNewUrlParser: true,
+// });
+// const db = mongoose.connection;
 
-app.listen("8000", () => {
-  console.log("Server is running on port: 8000");
+db.once('open', () => {
+  // console.log('Connected to the database');
 });
+db.on('error', (err) => console.log('Error ' + err));
+
+const server = app.listen('8000', () => {
+  // console.log('Server is running on port: 8000');
+});
+
+module.exports = server;
